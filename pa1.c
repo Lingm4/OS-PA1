cb52e252ef5c52504c2898ae964786f8b1752a08
윤문경 <yoonmoonkyung@yunmungyeong-ui-MacBookPro.local>
2023-04-06 00:44:30 +0900

    add alias

--- pa1.c
+++ pa1.c
@@ -18,6 +18,90 @@
 #include <sys/types.h>
 #include <unistd.h>
 #include <stdlib.h>
+#include "types.h"
+#include "list_head.h"
+
+/***********************************************************************
+ * list struct
+ *
+ * add_list_entry()
+ * remove_list_entry()
+ * RETURN VALUE
+ *   Return 1 on successful command execution
+ *   Return 0 when user inputs "exit"
+ *   Return <0 on error
+ */
+
+LIST_HEAD(alias_list);
+
+struct alias_list_entry{
+	int nr_origin;
+	char **origin;
+	char *replace;
+	struct list_head list;
+};
+
+void add_list_entry(char **const org, const int nr_org, char * const rep)
+{
+	struct alias_list_entry *new_entry = (struct alias_list_entry *)malloc(sizeof(struct alias_list_entry));
+	
+	new_entry->nr_origin = nr_org;
+	
+	new_entry->replace = (char *)malloc(sizeof(char)*strlen(rep) +1); // +1 for '\0'
+	strcpy(new_entry->replace, rep);
+
+	new_entry->origin = (char **)malloc(sizeof(char*)*nr_org);
+	for(int i = 0; i < nr_org; i++){
+		(new_entry->origin)[i] = (char *)malloc(sizeof(char)*strlen(org[i]) +1); // +1 for '\0'
+		strcpy((new_entry->origin)[i], org[i]);
+	}
+	list_add(&(new_entry->list), &alias_list);
+}
+
+void dump_alias_list(void)
+{
+	struct list_head *pos;
+	list_for_each_prev(pos, &alias_list){
+		fprintf(stderr, "%s:", list_entry(pos, struct alias_list_entry, list)->replace);
+		for(int i=0; i<list_entry(pos, struct alias_list_entry, list)->nr_origin; i++){
+			fprintf(stderr, " %s", (list_entry(pos, struct alias_list_entry, list)->origin)[i]);
+		}
+		fprintf(stderr, "\n");
+	}
+}
+
+struct alias_list_entry *find_alias_list_entry(const char *const target){
+	//printf("finding %s...\n", target); //지워야함
+	struct list_head *pos;
+	list_for_each_prev(pos, &alias_list){
+		if(strcmp(target, list_entry(pos, struct alias_list_entry, list)->replace)==0) return list_entry(pos, struct alias_list_entry, list);
+	}
+	return NULL;
+}
+
+void make_tokens_replacing_alias(char **tokens, int *nr_tokens){
+	for(int i=0; i<*nr_tokens; i++){
+		struct alias_list_entry *target_alias_entry = find_alias_list_entry(tokens[i]);
+		if(target_alias_entry != NULL){
+			//printf("replacing...\n");
+			for(int j=0; j<target_alias_entry->nr_origin-1; j++){
+				for(int k=0; k<*nr_tokens-i; k++){
+					//printf("push tokens...\n");
+					free(tokens[*nr_tokens-k]);
+					tokens[*nr_tokens-k] = strdup(tokens[*nr_tokens-k-1]);
+				}
+				(*nr_tokens)++;
+			}
+			for(int j=0; j<target_alias_entry->nr_origin; j++){
+				//printf("push origin...\n");
+				free(tokens[i+j]);
+				tokens[i+j] = strdup(target_alias_entry->origin[j]);
+			}
+			i+=target_alias_entry->nr_origin-1;
+		}
+	}
+}
+
 /***********************************************************************
  * run_command()
  *
@@ -34,7 +118,7 @@
 int run_command(int nr_tokens, char *tokens[])
 {
 	if (strcmp(tokens[0], "exit") == 0) return 0;
-	if(strcmp(tokens[0], "cd") == 0){
+	else if(strcmp(tokens[0], "cd") == 0){
 		if(nr_tokens == 1 || strcmp(tokens[1], "~") == 0){
 			chdir(getenv("HOME"));
 			return 1;
@@ -42,19 +126,30 @@ int run_command(int nr_tokens, char *tokens[])
 			chdir(tokens[1]);
 			return 1;
 		}
-	}
-	int pid = fork();
-	if(pid == 0){
-		execvp(tokens[0], &tokens[0]);
-		fprintf(stdout, "Unable to execute %s\n", tokens[0]); // 나중에 바꿔야됨
-		exit(1);
-	}else if(pid == -1){
-		fprintf(stdout, "Unable to execute %s\n", tokens[0]); // 나중에 바꿔야됨
-		return -1;
-	}else{
-		int status;
-		waitpid(pid, &status, 0);
+	}else if(strcmp(tokens[0], "alias") == 0){
+		if(nr_tokens == 1) dump_alias_list();
+		else add_list_entry(&tokens[2], nr_tokens-2, tokens[1]);
 		return 1;
+	}else{
+		make_tokens_replacing_alias(tokens, &nr_tokens);
+		/*
+		for(int i=0; i<nr_tokens; i++){
+			printf("%s ", tokens[i]);
+		}printf("\n");
+		*/
+		int pid = fork();
+		if(pid == 0){
+			execvp(tokens[0], &tokens[0]);
+			fprintf(stderr, "Unable to execute %s\n", tokens[0]);
+			exit(1);
+		}else if(pid == -1){
+			fprintf(stderr, "Unable to execute %s\n", tokens[0]);
+			return -1;
+		}else{
+			int status;
+			waitpid(pid, &status, 0);
+			return 1;
+		}
 	}
 }
 
